2223782044479ee71dcecc9df9ac1072c06969e4
QuocDL <quoclcph18659@gmail.com>
2026-02-09 09:17:22 +0700

    Day 6: From state

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { useState } from "react";
 
 const LoginPage = () => {
-  return <div>Dang nhap</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ email, password });
+  };
+  return (
+    <div
+      style={{
+        maxWidth: 720,
+        margin: "0 auto",
+      }}
+    >
+      <h1>Đăng nhập</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label htmlFor="">Email</label>
+          <input
+            type="text"
+            placeholder="Nhập email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div
+          className="form-field"
+          style={{
+            marginTop: 8,
+          }}
+        >
+          <label htmlFor="">Password</label>
+          <input
+            type="password"
+            placeholder="Nhap password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          style={{
+            marginTop: 14,
+          }}
+        >
+          Đăng nhập
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
