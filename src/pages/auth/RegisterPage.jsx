3ca9d132f32b46ad969c8ead2aff933326cfee89
QuocDL <quoclcph18659@gmail.com>
2026-02-09 13:32:20 +0700

    Day 6: Zod validate

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,6 +1,21 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { data, useNavigate } from "react-router";
+import z from "zod";
+import { da } from "zod/locales";
+
+const registerSchema = z
+  .object({
+    email: z.email("Email không hợp lệ"),
+    password: z.string().min(6, "Yêu cầu trên 6 ký tự"),
+    gender: z.string().min(1, "Vui lòng chọn giới tính"),
+    confirmPassword: z.string().min(6, "Yêu cầu trên 6 ký tự"),
+  })
+  .refine((data) => data.confirmPassword === data.password, {
+    path: ["confirmPassword"],
+    message: "Mật khẩu xác nhận không giống",
+  });
 
 const RegisterPage = () => {
   const nav = useNavigate();
@@ -8,7 +23,9 @@ const RegisterPage = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    resolver: zodResolver(registerSchema),
+  });
   const onSubmit = async (data) => {
     try {
       await axios.post(
@@ -33,11 +50,7 @@ const RegisterPage = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-field">
           <label htmlFor="">Email</label>
-          <input
-            type="text"
-            placeholder="Nhập email"
-            {...register("email", { required: "Yêu cầu nhập Email" })}
-          />
+          <input type="text" placeholder="Nhập email" {...register("email")} />
           {errors.email && (
             <p style={{ color: "red", margin: 0 }}>{errors?.email?.message}</p>
           )}
@@ -50,19 +63,45 @@ const RegisterPage = () => {
         >
           <label htmlFor="">Password</label>
           <input
-            type="password"
+            type="text"
             placeholder="Nhap password"
-            {...register("password", { required: "Yêu cầu nhập mật khẩu" })}
+            {...register("password")}
           />
+          {errors.password && (
+            <p style={{ color: "red", margin: 0 }}>
+              {errors?.password?.message}
+            </p>
+          )}
+        </div>
+        <div
+          className="form-field"
+          style={{
+            marginTop: 8,
+          }}
+        >
+          <label htmlFor="">Confirm Password</label>
+          <input
+            type="text"
+            placeholder="Nhap password"
+            {...register("confirmPassword")}
+          />
+          {errors.confirmPassword && (
+            <p style={{ color: "red", margin: 0 }}>
+              {errors?.confirmPassword?.message}
+            </p>
+          )}
         </div>
         <div>
           <label htmlFor="">Gioi tinh</label>
-          <select name="" id="" {...register("gender", { required: true })}>
+          <select name="" id="" {...register("gender")}>
             <option value="">Chon gioi tinh</option>
             <option value="Male">Nam</option>
             <option value="Female">Nu</option>
             <option value="other">khac</option>
           </select>
+          {errors.gender && (
+            <p style={{ color: "red", margin: 0 }}>{errors?.gender?.message}</p>
+          )}
         </div>
         <button
           style={{
